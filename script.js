0ba3ded311e7b31cbbdf67fa9dde2fa9c507c77a
Matthew <102175343+Long-Hold@users.noreply.github.com>
2025-03-25 14:28:06 -0400

    feat: Implement match winner algorithm

--- script.js
+++ script.js
@@ -40,11 +40,55 @@ function getHumanChoice()
     return prompt("Rock, Paper, Scissors: ").toLowerCase();
 }
 
-function playRound(humanChoice, getComputerChoice)
+function playRound(humanChoice, ComputerChoice)
 {
-    //
+    // Compare human and computer choice to eachother
+    // Based on game rules, increment the score of the winner
+    // Display a win or lose message for the human player
+    if (isHumanWinner(humanChoice, ComputerChoice))
+    {
+        alert("You win!");
+        ++humanScore;
+    }
+    else
+    {
+        alert("You lose!");
+        ++computerScore;
+    }
+    return;
+}
+
+function isHumanWinner(humanChoice, ComputerChoice)
+{
+    // Compare the human and computer plays
+    // Return True if Human wins
+    // Return False if computer wins (Human player loses)
+    if (humanChoice === 'rock')
+    {
+        if (ComputerChoice === 'scissors')
+        {
+            return true;
+        }
+    }
+    else if (humanChoice === 'paper')
+    {
+        if (ComputerChoice === 'rock')
+        {
+            return true;
+        }
+    }
+    else if (humanChoice === 'scissors')
+    {
+        if (ComputerChoice === 'paper')
+        {
+            return true;
+        }
+    }
+    else return false;
 }
 
 // const humanSelection = getHumanChoice();
-// const computerSelection = getComputerChoice();
+const computerSelection = getComputerChoice();
+
+// playRound(humanSelection, computerSelection);
 
